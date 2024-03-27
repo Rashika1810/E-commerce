248c40c88f9d01c94dd6922359652ffe1824144c
Rashika1810 <rashika.kr04@gmail.com>
2024-03-27 15:06:38 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
